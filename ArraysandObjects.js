cfff4e63a4beb88155c098b6e57ca6f3c894a455
Emma <eeg4@uw.edu>
2022-05-30 15:04:37 -0700

    Big O Object Operations Exercises

---
+++ ArraysandObjects.js
@@ -0,0 +1,9 @@
+/*Object Operations Exercises*/
+// 1) What is the big O for adding a key and value into an object?
+// O (1)
+
+// 2) What is the big O for accessing a key in an object?
+// O (1)
+
+// 3) What is the big O of removing a key in an object
+// O (1)
