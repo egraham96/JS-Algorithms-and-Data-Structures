3c66fc5065e438e50b023ae43d3fa109396ed6df
Emma <eeg4@uw.edu>
2022-05-30 20:01:14 -0700

    Recursion Practice

---
+++ Recursion.js
@@ -0,0 +1,6 @@
+/*Recursions Exercises*/
+// 1) What tool does JavaScript use to manage function invocations?
+// The call stack
+
+// 2) What is the base case?
+// A situation when the recursion ends
